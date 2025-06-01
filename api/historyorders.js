1575f5d802779c6e63f46dea16bba1bec7b69ce2
konata0721 <ijn_0327@qq.com>
2025-06-01 20:44:55 +0800

    Update historyorders.js

--- api/historyorders.js
+++ api/historyorders.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
 
     // 查询 historyorders 表
     const [rows] = await connection.execute(
-      'SELECT id, `订单时间`, account, `交易名称` FROM historyorders WHERE account = ?',
+      'SELECT id, ordertime, account, ordername FROM historyorders WHERE account = ?',
       [account]
     );
 
